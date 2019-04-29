43d3ec84196e7673274e51439518c81c7fa96112
J Young Kim <jyoungk@andrew.cmu.edu>
2019-04-28 21:32:08 -0400

    Update Store Page

--- code/js/scripts.js
+++ code/js/scripts.js
@@ -1,17 +1,5 @@
-function validate() {
-    var zipcode = $("#zip").val();
-    var zipstr = zipcode.toString();
-    if (zipstr.length != 5) {
-        alert("Please enter 5 digit zip code!");
-        return false;
-    }
+function changeWallet(id, s) {
+    var image = document.getElementById(id);
+    image.src = s
 }
 
-function validatePhone() {
-    var phoneNumber = $("#phone").val();
-    console.log(isNaN(phoneNumber));
-    if (!isNaN(phoneNumber) || phoneNumber.length != 10) {
-        alert("Invalid phone number. If you want yo pie yo better give me the right number boy.");
-        return false;
-    }
-}
