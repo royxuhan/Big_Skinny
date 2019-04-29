00284687f7f301a6785caac992a7fb99a51fb007
J Young Kim <jyoungk@andrew.cmu.edu>
2019-04-28 22:20:41 -0400

    Finished StorePage

--- code/js/scripts.js
+++ code/js/scripts.js
@@ -3,3 +3,18 @@ function changeWallet(id, s) {
     image.src = s
 }
 
+function colorSelect(s) {
+	document.getElementById("sBrown").classList.remove("selectedColor");
+	document.getElementById("sBlack").classList.remove("selectedColor");
+	document.getElementById("sGray").classList.remove("selectedColor");
+	document.getElementById("sBlue").classList.remove("selectedColor");
+	document.getElementById(s).classList.add("selectedColor");
+}
+
+function changeColors(s) {
+	document.getElementById("blackWallet").classList.add("hide");
+	document.getElementById("grayWallet").classList.add("hide");
+	document.getElementById("brownWallet").classList.add("hide");
+	document.getElementById("blueWallet").classList.add("hide");
+	document.getElementById(s).classList.remove("hide");
+}
